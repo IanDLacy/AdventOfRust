7dcf0f8ba33c0d34c10129d0026fc60167c95503
IanDLacy <Ian.D.Lacy@Gmail.com>
2022-12-17 00:53:24 -0500

    Day 2

--- 22/src/d02.rs
+++ 22/src/d02.rs
@@ -1,11 +1,45 @@
-//use crate::basics::*;
+use crate::basics::*;
 
 pub fn p1() -> u32 {
-    //let mut lines = lines(path(2));
-    0
+    let lines = lines(path(2));
+    let mut score: u32 = 0;
+    for line in lines {
+        let line = line.expect("Expected To Read Line");
+        let mut chars = line.chars();
+        score += match (chars.next().unwrap(), chars.last().unwrap()) {
+            ('B', 'X') => 0 + 1,
+            ('C', 'Y') => 0 + 2,
+            ('A', 'Z') => 0 + 3,
+            ('A', 'X') => 3 + 1,
+            ('B', 'Y') => 3 + 2,
+            ('C', 'Z') => 3 + 3,
+            ('C', 'X') => 6 + 1,
+            ('A', 'Y') => 6 + 2,
+            ('B', 'Z') => 6 + 3,
+            _ => 0,
+        }
+    }
+    score
 }
 
 pub fn p2() -> u32 {
-    //let mut lines = lines(path(2));
-    0
+    let lines = lines(path(2));
+    let mut score: u32 = 0;
+    for line in lines {
+        let line = line.expect("Expected To Read Line");
+        let mut chars = line.chars();
+        score += match (chars.next().unwrap(), chars.last().unwrap()) {
+            ('B', 'X') => 0 + 1,
+            ('C', 'X') => 0 + 2,
+            ('A', 'X') => 0 + 3,
+            ('A', 'Y') => 3 + 1,
+            ('B', 'Y') => 3 + 2,
+            ('C', 'Y') => 3 + 3,
+            ('C', 'Z') => 6 + 1,
+            ('A', 'Z') => 6 + 2,
+            ('B', 'Z') => 6 + 3,
+            _ => 0,
+        }
+    }
+    score
 }
