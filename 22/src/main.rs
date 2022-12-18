e7992c9031d50100c6542259e229c35026a7fe36
IanDLacy <Ian.D.Lacy@Gmail.com>
2022-12-18 18:23:25 -0500

    Clean Up Main

--- 22/src/main.rs
+++ 22/src/main.rs
@@ -1,26 +1,27 @@
 mod modules;
 
+use modules::*;
 use std::env;
 
 fn main() {
     match env::args().nth(1).unwrap_or_default().as_str() {
         "go" => {
-            modules::commands::cargo_build();
-            modules::commands::git_add();
-            modules::commands::git_commit();
-            modules::commands::git_push();
+            commands::cargo_build();
+            commands::git_add();
+            commands::git_commit();
+            commands::git_push();
         }
         "1" => {
-            modules::commands::answer(modules::d01::p1());
-            modules::commands::answer(modules::d01::p2());
+            commands::answer(d01::p1());
+            commands::answer(d01::p2());
         }
         "2" => {
-            modules::commands::answer(modules::d02::p1());
-            modules::commands::answer(modules::d02::p2());
+            commands::answer(d02::p1());
+            commands::answer(d02::p2());
         }
         "3" => {
-            modules::commands::answer(modules::d03::p1());
-            modules::commands::answer(modules::d03::p2());
+            commands::answer(d03::p1());
+            commands::answer(d03::p2());
         }
         _ => {}
     }
