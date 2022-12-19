af4b0e9d1ee6cb58bcde70b635cdda59a252a9a0
IanDLacy <Ian.D.Lacy@Gmail.com>
2022-12-18 23:55:15 -0500

    Clean Up Function 'zsh'

--- src/commands.rs
+++ src/commands.rs
@@ -3,16 +3,16 @@ use std::env;
 use super::basics;
 
 pub fn zsh() {
-    let dir = env::current_dir().expect("Expected Current Directory");
-    let dir = dir
-        .to_str()
-        .expect("Expected Successful Cast To String Slice");
-
-    let path =
-        env::var("PATH").expect("Expected Environment Variable") + ":" + dir + "/target/debug";
-
-    env::set_var("PATH", path);
-
+    let dir = env::current_dir()
+        .expect("Expected Current Directory")
+        .join("target/debug");
+    let path = env::var_os("PATH").expect("Expected Environment Variable");
+    let mut paths = env::split_paths(&path).collect::<Vec<_>>();
+    paths.push(dir);
+    env::set_var(
+        "PATH",
+        env::join_paths(paths).expect("Expected To Join Paths"),
+    );
     basics::command("zsh", &["-l"]);
 }
 
