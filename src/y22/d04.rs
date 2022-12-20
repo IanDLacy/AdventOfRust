4e5cb56872d45897d7cf6133453c2d1107ba1ccb
IanDLacy <Ian.D.Lacy@Gmail.com>
2022-12-19 23:44:24 -0500

    Day 4

--- src/y22/d04.rs
+++ src/y22/d04.rs
@@ -1,17 +1,35 @@
 use crate::basics::{input, lines};
 
 pub fn p1() -> u32 {
-    let value = 0;
+    let mut count = 0;
     for line in lines(input(22, 4)) {
-        line.unwrap();
+        let line = line.expect("Expected To Read Line");
+        let elves: Vec<&str> = line.split(',').collect();
+        let first: Vec<u8> = elves[0].split('-').map(|i| i.parse().unwrap()).collect();
+        let second: Vec<u8> = elves[1].split('-').map(|i| i.parse().unwrap()).collect();
+        if first[0] >= second[0] && first[1] <= second[1]
+            || first[0] <= second[0] && first[1] >= second[1]
+        {
+            count += 1;
+        }
     }
-    value
+    count
 }
 
 pub fn p2() -> u32 {
-    let value = 0;
+    let mut count = 0;
     for line in lines(input(22, 4)) {
-        line.unwrap();
+        let line = line.expect("Expected To Read Line");
+        let elves: Vec<&str> = line.split(',').collect();
+        let first: Vec<u8> = elves[0].split('-').map(|i| i.parse().unwrap()).collect();
+        let second: Vec<u8> = elves[1].split('-').map(|i| i.parse().unwrap()).collect();
+        if first[1] <= second[1] && first[1] >= second[0]
+            || first[0] <= second[1] && first[0] >= second[0]
+            || second[1] <= first[1] && second[1] >= first[0]
+            || second[0] <= first[1] && second[0] >= first[0]
+        {
+            count += 1;
+        }
     }
-    value
+    count
 }
