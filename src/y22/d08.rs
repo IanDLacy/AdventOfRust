193edc307b0687145be865671bb75f15c217e82c
IanDLacy <Ian.D.Lacy@Gmail.com>
2022-12-23 02:53:07 -0500

    Day 8 Part 1

--- src/y22/d08.rs
+++ src/y22/d08.rs
@@ -1,10 +1,79 @@
 use crate::basics::{input, lines};
 
-pub fn p1() -> String {
-    for line in lines(input(22, 8)) {
-        line.expect("Expected To Read Line");
+struct Tree {
+    visible: bool,
+    height: u8,
+}
+
+fn rows() -> Vec<Vec<Tree>> {
+    let mut rows: Vec<Vec<Tree>> = Vec::new();
+    for (row, line) in lines(input(22, 8)).enumerate() {
+        rows.push(Vec::new());
+        let line = line.expect("Expected To Read Line");
+        for char in line.chars() {
+            rows[row].push(Tree {
+                visible: false,
+                height: char.to_digit(10).unwrap() as u8,
+            })
+        }
     }
-    "answer".to_owned()
+    rows
+}
+
+fn cols(rows: Vec<Vec<Tree>>) -> Vec<Vec<Tree>> {
+    let mut cols: Vec<Vec<Tree>> = Vec::new();
+    for row in rows {
+        for (col, tree) in row.iter().enumerate() {
+            if col + 1 > cols.len() {
+                cols.push(Vec::new());
+            }
+            cols[col].push(Tree {
+                visible: tree.visible,
+                height: tree.height,
+            });
+        }
+    }
+    cols
+}
+
+fn scan(trees: Vec<Tree>) -> Vec<Tree> {
+    let mut scanned: Vec<Tree> = Vec::new();
+    let mut max: u8 = 0;
+    for tree in trees {
+        if tree.height + 1 > max {
+            max = tree.height + 1;
+            scanned.push(Tree {
+                visible: true,
+                height: tree.height,
+            });
+        } else {
+            scanned.push(Tree {
+                visible: tree.visible,
+                height: tree.height,
+            });
+        }
+    }
+    scanned
+}
+
+fn look(vec: Vec<Vec<Tree>>) -> Vec<Vec<Tree>> {
+    let mut looked: Vec<Vec<Tree>> = Vec::new();
+    for mut item in vec {
+        item = scan(item);
+        item.reverse();
+        item = scan(item);
+        looked.push(item);
+    }
+    looked
+}
+
+pub fn p1() -> String {
+    look(cols(look(rows())))
+        .iter()
+        .flatten()
+        .map(|tree| if tree.visible { 1 } else { 0 })
+        .sum::<u32>()
+        .to_string()
 }
 
 pub fn p2() -> String {
